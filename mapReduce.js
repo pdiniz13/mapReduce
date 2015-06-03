823821c436348730a445fe4cc7621593766b8374
Paulo Diniz <pdiniz_100@yahoo.com>
2015-06-02 18:07:23 -0700

    WIP-Trying to reduce the the ammoung of repeated code for map/reduce function

--- mapReduce.js
+++ mapReduce.js
@@ -1,44 +1,74 @@
-var conn = new Mongo("127.0.0.1:3001");
-var db = conn.getDB("meteor");
+var mapReduce = function (outputFile, range) {
+  var conn = new Mongo("127.0.0.1:3001");
+  var db = conn.getDB("meteor");
 
-var mapFunc = function() {
-  emit(this.serviceTypeID, {count: 1, serviceTime: this.serviceTime, products: this.products});
-};
+  var mapFunc = function () {
+    emit(this.serviceTypeID, {count: 1, serviceTime: this.serviceTime, products: this.products});
+  };
 
-var reduceFunc = function(serviceTypeID, info) {
-  var reducedVal = {count: 1, serviceTime: 0, totalProductsCount: 0, totalPerItem: {}, ratingTotal:0};
-  for (var idx = 0, count = info.length; idx < count; idx++) {
-    reducedVal.count += info[idx].count;
-    reducedVal.serviceTime += info[idx].serviceTime;
-    reducedVal.ratingTotal += info[idx].ratingTotal;
-    if (info[idx].products) {
-      reducedVal.totalProductsCount += info[idx].products.length - 1;
-      for (var x = 0, count1 = info[idx].products.length; x < count1; x++) {
-        if (reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]) {
-          reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]++;
+  var reduceFunc = function (serviceTypeID, info) {
+    var reducedVal = {count: 1, serviceTime: 0, totalProductsCount: 0, totalPerItem: {}, ratingTotal: 0};
+    for (var idx = 0, count = info.length; idx < count; idx++) {
+      reducedVal.count += info[idx].count;
+      reducedVal.serviceTime += info[idx].serviceTime;
+      reducedVal.ratingTotal += info[idx].ratingTotal;
+      if (info[idx].products) {
+        reducedVal.totalProductsCount += info[idx].products.length - 1;
+        for (var x = 0, count1 = info[idx].products.length; x < count1; x++) {
+          if (reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]) {
+            reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]++;
+          }
+          else {
+            reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]] = 0;
+          }
         }
-        else {
-          reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]] = 0;
-        }
+      }
+    }
+
+    return reducedVal;
+  };
+
+  var finalizeFunc = function (key, reducedVal) {
+    reducedVal.serviceTimeAvg = reducedVal.serviceTime / reducedVal.count;
+    reducedVal.rating = reducedVal.ratingTotal / reducedVal.count;
+    return reducedVal;
+  };
+
+  var queryOptions = {
+    out:outputFile,
+    finalize: finalizeFunc
+  };
+  if (range){
+    queryOptions.query = {
+      updatedAt: {
+        $gt: range[0],
+        $lt: range[1]
       }
     }
   }
 
-  return reducedVal;
+  db.tickets.mapReduce(
+    mapFunc,
+    reduceFunc,
+    queryOptions
+  );
 };
 
-var finalizeFunc = function (key, reducedVal) {
-  reducedVal.serviceTimeAvg = reducedVal.serviceTime/reducedVal.count;
-  reducedVal.rating = reducedVal.ratingTotal/reducedVal.count;
-  return reducedVal;
+mapReduce("allServicesStats");
+var dateRanges = {
+  januaryServiceStats2015: [new Date(2015,0,1), new Date (2015,0,31)],
+  februaryServiceStats2015: [new Date(2015,1,1), new Date (2015,1,28)],
+  marchServiceStats2015: [new Date(2015,2,1), new Date (2015,2,31)],
+  aprilServiceStats2015: [new Date(2015,3,1), new Date (2015,3,30)],
+  mayServiceStats2015: [new Date(2015,4,1), new Date (2015,4,31)],
+  juneServiceStats2015: [new Date(2015,5,1), new Date (2015,5,30)],
+  julyServiceStats2015: [new Date(2015,6,1), new Date (2015,6,31)],
+  augustServiceStats2015: [new Date(2015,7,1), new Date (2015,7,31)],
+  septemberServiceStats2015: [new Date(2015,8,1), new Date (2015,8,30)],
+  octoberServiceStats2015: [new Date(2015,9,1), new Date (2015,9,31)],
+  novemberServiceStats2015: [new Date(2015,10,1), new Date (2015,10,30)],
+  decemberServiceStats2015: [new Date(2015,11,1), new Date (2015,11,31)]
 };
-
-
-db.tickets.mapReduce(
-  mapFunc,
-  reduceFunc,
-  {
-    out: "allServicesStats",
-    finalize: finalizeFunc
-  }
-);
+for (key in dateRanges){
+  mapReduce(key, dateRanges[key]);
+}
--- mapReduceUser.js
+++ mapReduceUser.js
@@ -1,42 +1,80 @@
-var conn = new Mongo("127.0.0.1:3001");
-var db = conn.getDB("meteor");
+var mapReduce = function(indexName, outputFile, range) {
+  var conn = new Mongo("127.0.0.1:3001");
+  var db = conn.getDB("meteor");
 
-var mapFunc = function() {
-  emit(this.agentID, {count: 1, serviceTime: this.serviceTime, products: this.products, agent: this.agentID});
-};
+  var mapFunc = function (indexName) {
+    emit(this[indexName], {count: 1, serviceTime: this.serviceTime, products: this.products});
+  };
 
-var reduceFunc = function(agentID, info) {
-  var reducedVal = {count: 1, serviceTime: 0, totalProductsCount: 0, totalPerItem: {}, ratingTotal:0};
-  for (var idx = 0, count = info.length; idx < count; idx++){
-    reducedVal.count += info[idx].count;
-    reducedVal.serviceTime += info[idx].serviceTime;
-    reducedVal.ratingTotal += info[idx].ratingTotal;
-    if (info[idx].products) {
-      reducedVal.totalProductsCount += info[idx].products.length - 1;
-      for (var x = 0, count1 = info[idx].products.length; x < count1; x++) {
-        if (reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]) {
-          reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]++;
-        }
-        else {
-          reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]] = 0;
+  var reduceFunc = function (agentID, info) {
+    var reducedVal = {count: 1, serviceTime: 0, totalProductsCount: 0, totalPerItem: {}, ratingTotal: 0};
+    for (var idx = 0, count = info.length; idx < count; idx++) {
+      reducedVal.count += info[idx].count;
+      reducedVal.serviceTime += info[idx].serviceTime;
+      reducedVal.ratingTotal += info[idx].ratingTotal;
+      if (info[idx].products) {
+        reducedVal.totalProductsCount += info[idx].products.length - 1;
+        for (var x = 0, count1 = info[idx].products.length; x < count1; x++) {
+          if (reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]) {
+            reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]]++;
+          }
+          else {
+            reducedVal.totalPerItem[reducedVal.totalPerItem[info[idx].products[x]]] = 0;
+          }
         }
       }
     }
-  }
-  return reducedVal;
-};
+    return reducedVal;
+  };
 
-var finalizeFunc = function (key, reducedVal) {
-  reducedVal.serviceTimeAvg = reducedVal.serviceTime/reducedVal.count;
-  reducedVal.rating = reducedVal.ratingTotal/reducedVal.count;
-  return reducedVal;
-};
+  var finalizeFunc = function (key, reducedVal) {
+    reducedVal.serviceTimeAvg = reducedVal.serviceTime / reducedVal.count;
+    reducedVal.rating = reducedVal.ratingTotal / reducedVal.count;
+    return reducedVal;
+  };
 
-db.tickets.mapReduce(
-  mapFunc,
-  reduceFunc,
-  {
-    out: "allUsersStats",
+  var queryOptions = {
+    out:outputFile,
     finalize: finalizeFunc
+  };
+  if (range){
+    queryOptions.query = {
+      updatedAt: {
+        $gt: range[0],
+        $lt: range[1]
+      }
+    }
   }
-);
+  var newFunc = function(indexName){
+    var indexName = indexName;
+    function mapFunc(){
+      var newName = indexName;
+      emit(this[newName], {count: 1, serviceTime: this.serviceTime, products: this.products});
+    }
+    return mapFunc;
+  };
+  db.tickets.mapReduce(
+    newFunc(indexName),
+    reduceFunc,
+    queryOptions
+  );
+};
+
+mapReduce("agentID", "allAgentStats");
+var dateRanges = {
+  januaryAgentStats2015: [new Date(2015,0,1), new Date (2015,0,31)],
+  februaryAgentStats2015: [new Date(2015,1,1), new Date (2015,1,28)],
+  marchAgentStats2015: [new Date(2015,2,1), new Date (2015,2,31)],
+  aprilAgentStats2015: [new Date(2015,3,1), new Date (2015,3,30)],
+  mayAgentStats2015: [new Date(2015,4,1), new Date (2015,4,31)],
+  juneAgentStats2015: [new Date(2015,5,1), new Date (2015,5,30)],
+  julyAgentStats2015: [new Date(2015,6,1), new Date (2015,6,31)],
+  augustAgentStats2015: [new Date(2015,7,1), new Date (2015,7,31)],
+  septemberAgentStats2015: [new Date(2015,8,1), new Date (2015,8,30)],
+  octoberAgentStats2015: [new Date(2015,9,1), new Date (2015,9,31)],
+  novemberAgentStats2015: [new Date(2015,10,1), new Date (2015,10,30)],
+  decemberAgentStats2015: [new Date(2015,11,1), new Date (2015,11,31)]
+};
+for (key in dateRanges){
+  mapReduce("agentID", key, dateRanges[key]);
+}
